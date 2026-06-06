e39936cf21daec6749b29a16244fe01da44c5be0
Abhay Negi <negiabhay2006@gmail.com>
2026-06-06 15:03:21 +0530

    Refactor app.py to improve structure and execution

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 def add(a, b):
     return a + b
 
-print("App is running...")
-print(add(2, 3))
-
-user_input = input()
-eval(user_input)
-
 AWS_SECRET_KEY = "AKIA123456789TEST"
+
+if __name__ == "__main__":
+    print("App is running...")
+    print(add(2, 3))
+
+    user_input = input()
+    eval(user_input)
